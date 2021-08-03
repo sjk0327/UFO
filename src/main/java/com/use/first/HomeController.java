e24844b606e02cdf34eb2dffba403e7aa4d47f3c
been <jisb159@gmail.com>
2021-08-03 18:05:47 +0900

    adminlogin success

--- src/main/java/com/use/first/HomeController.java
+++ src/main/java/com/use/first/HomeController.java
@@ -4,18 +4,27 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.use.first.member.UserDAO;
+import com.use.first.member.UserVO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -47,7 +56,61 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "admintest";
+		return "adminLogin";
 	}
 
+	
+	
+	
+//
+//	@RequestMapping(value = "/adminMemList", method = RequestMethod.GET)
+//	public String adminMenList(Locale locale, Model model) {
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "adminMenList";
+//	}
+//	
+//	
+	
+	
+	@RequestMapping(value="/admin", method=RequestMethod.GET)
+	public String adminLogin(UserVO user, Model model) {
+		model.addAttribute("user", user);
+		return "/enterance/adminLogin";
+	}
+	
+	
+	
+	@RequestMapping(value="/admin", method=RequestMethod.POST)
+	public String adminLogin(UserVO vo, HttpSession session) {
+		if(vo.getM_id() == null || vo.getM_id().equals("")) {
+			System.out.println("여기까지");
+			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
+		}
+		UserDAO dao = sqlSessionTemplate.getMapper(UserDAO.class);
+		UserVO user = dao.memInfo(vo.getM_id());
+
+		if(user != null) {
+			session.setAttribute("userName", user.getM_id());
+			return "/admin/mem/adminMenList";
+		} else {
+			return "/enterance/adminLogin";
+		}
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/com/use/first/member/UserVO.java
@@ -0,0 +1,27 @@
+package com.use.first.member;
+
+import java.util.Date;
+
+public class UserVO {
+	
+	private String m_id;
+
+	private String m_pw;
+
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	
+	
+
+}
