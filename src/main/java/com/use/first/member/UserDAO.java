eeab321ad7b58eca79051d09f1ee215a06bdd28b
been <jisb159@gmail.com>
2021-08-18 20:09:23 +0900

    been0818

--- src/main/java/com/use/first/member/UserDAO.java
+++ src/main/java/com/use/first/member/UserDAO.java
@@ -22,8 +22,8 @@ public interface UserDAO {
 	public int userDeleteUpdateAlert(@Param("a_mid") String a_mid);
 	public int userDeleteUpdateWish(@Param("w_mid") String w_mid);
 	
-//	public int userDeleteUpdateBuy(@Param("b_mid") String b_mid);
-//	public int userDeleteUpdateRental(@Param("r_mid") String r_mid);
+	public int userDeleteUpdateBuy(@Param("b_mid") String b_mid);
+	public int userDeleteUpdateRental(@Param("r_mid") String r_mid);
 	public int userDeleteUpdateReview(@Param("v_mid") String v_mid);
 	
 	public int userConfirm(@Param("r_mid") String r_mid);
